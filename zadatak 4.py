4bcc00d1a45f3dd218ad9bd45a3063567e0fecae
Marin Katana-Martić <mkmartic@gmail.com>
2023-03-14 19:40:04 +0100

    prvi labos

---
+++ zadatak 4.py
@@ -0,0 +1,13 @@
+# Ova linija mora biti komentar!
+print('Dobrodošli', '!'*3)
+# Napisati sljedeće izraze u jednoj liniji koda
+print('Unesite vaše ime: ')
+ime = input()
+print('Unesite vaše prezime: ')
+prezime = input()
+print('Unesite vaš JMBAG: ')
+jmbag = input()
+print('Unesite vašu dob: ')
+godine = input()
+# TODO ime i prezime ispišite u navodnim znakovima
+print("Korisnik : ", "'", ime,"'", " ", "'", prezime, "'", " ", godine, end="!")
